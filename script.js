3b9a988ac261c60173f6cc84c85ec06d13bcbb56
Natalie Browne Holmes <nbh325@gmail.com>
2019-03-16 17:14:47 -0400

    .....

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-let hi = "hello"
-console.log(hi);
+let rain = null;
+let sunny = null;
+
+let pageTitle= document.getElementById("header");
+let pageTitleText = pageTitle.innterHTML;
+let tryAgain = document.getElementById ("try again");
+let quizWrapper = document.getElementById("quiz-wrapper");
+let formSubmit = dodocument.getElementById ("submit");
+
+function processresults(){
+	console.log ("This is process results")
+}
+function getPersonality(){
+	console.log ("get personality")
+}
+function resetQuiz (){
+	console.log ("reset quiz")
+}
+
+tryAgain.addEventListener.click(resetquiz)
+formSubmit.addEventListener.click (processResults)
